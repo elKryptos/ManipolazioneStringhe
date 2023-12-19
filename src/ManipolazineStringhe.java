a3b6beca44677615e3b9a8d3424cc6847909e42a
Hans Sidney De La Cruz Mancisidor <hans.delacruz@itsrizzoli.it>
2023-12-19 10:51:18 +0100

    commit iniziale

---
+++ src/ManipolazineStringhe.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ManipolazineStringhe {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digita una frase: ");
+        String txt = scanner.nextLine();
+   
+    }
+}
