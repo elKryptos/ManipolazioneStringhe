158ff87e7a23d4d2dfc022328037e279281a690a
Hans Sidney De La Cruz Mancisidor <hans.delacruz@itsrizzoli.it>
2023-12-19 11:33:39 +0100

    minus

--- src/ManipolazineStringhe.java
+++ src/ManipolazineStringhe.java
@@ -7,11 +7,25 @@ public class ManipolazineStringhe {
         String txt = scanner.nextLine();
 
         int lunghezza = txt.length();
+        String maiuscolo = txt.toUpperCase();
+        String minuscolo = txt.toLowerCase();
 
         System.out.println("La lunghezza della tua stringa è: " + lunghezza );
+        System.out.println("La tua stringa in maiuscolo: " + maiuscolo );
+        System.out.println("La tua stringa in minuscolo: " + minuscolo );
     }
 
     public static int lunghezza (String txt){
         return + txt.length();   
     }
+
+    public static String maiuscolo (String txt){
+        return + txt.toUpperCase();   
+    }
+
+    public static String minuscolo (String txt){
+        return + txt.toLowerCase();   
+    }
+
+    
 }
